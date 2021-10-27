fe3a5ea60850bf2b28fecfa386acc7eeb796f4dc
Gabriel R <gabriel.ribeiro@argo-int.com>
2021-10-26 22:47:50 -0300

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const uuid = require('uuid')
+const dateFNS = require('date-fns')
 
 const app = express()
 
@@ -8,7 +9,65 @@ const validation = require('./validation')
 
 const payments = []
 
-app.post('/sendPaymnet', (req, res) => {
+const routes = {
+  SEND_PAYMENT: '/envioPagamento',
+  PROOF_OF_PAYMENT: '/comprovantePagamento',
+  VOUCHER_QUERY: '/consultaComprovante',
+  STATEMENT_PAYMENTS: '/extratoPagamentos',
+}
+
+app.get('/', (req, res) => {
+  try {
+    return res.status(200).send({
+      endpoints: [
+        {
+          path: routes.SEND_PAYMENT,
+          method: 'post',
+          body: {
+            CNPJ: 'string',
+            COD_BANCO: 'string',
+            AGENCIA: 'string',
+            CONTA: 'string',
+            EMPRESA: 'string',
+            TIPO_PAGAMENTO: 'string',
+            NOSSO_NUMERO: 'string',
+            DATA_VENC: 'string',
+            MOEDA: 'string',
+            VALOR: 'string',
+            IDENT_EXTRATO: 'string',
+          },
+        },
+        {
+          path: routes.PROOF_OF_PAYMENT,
+          method: 'get',
+          query: {
+            ID_AUTENTICACAO: 'string',
+          },
+        },
+        {
+          path: routes.VOUCHER_QUERY,
+          method: 'get',
+          query: {
+            ID_AUTENTICACAO: 'string',
+          },
+        },
+        {
+          path: routes.STATEMENT_PAYMENTS,
+          method: 'get',
+          query: {
+            CNPJ: 'string',
+            DATA_INI: 'date',
+            DATA_FIM: 'date',
+          },
+        },
+      ],
+    })
+  } catch (error) {
+    return res.status(500).send(error)
+  }
+})
+
+app.post(routes.SEND_PAYMENT, (req, res) => {
   try {
     // const envio_pagamento = {
     //   CNPJ: '',
@@ -35,21 +94,12 @@ app.post('/sendPaymnet', (req, res) => {
 })
 
 // comprovanteDePagamento
-app.get('/proofOfPayment', (req, res) => {
+app.get(routes.PROOF_OF_PAYMENT, (req, res) => {
   try {
     // const comprovante_pagamento = {
     //   ID_AUTENTICACAO: '',
     //   CNPJ: '',
-    //   COD_BANCO: '',
-    //   AGENCIA: '',
-    //   CONTA: '',
-    //   EMPRESA: '',
-    //   DATA_PGTO: '',
-    //   HORA_PGTO: '',
-    //   DATA_VENC: '',
-    //   VALOR: '',
-    //   VALOR_DESCONTO: '',
-    //   IDENT_EXTRATO: '',
+    //   COD_BANCO: '',CNPJ
     // }
     return res.status(200).send({ message: 'olá mundo' })
   } catch (error) {
@@ -58,7 +108,7 @@ app.get('/proofOfPayment', (req, res) => {
 })
 
 // consultaComprovante
-app.get('/voucherQuery', (req, res) => {
+app.get(routes.VOUCHER_QUERY, (req, res) => {
   try {
     // const consulta_comprovante = {
     //   ID_AUTENTICACAO: '',
@@ -83,8 +133,17 @@ app.get('/voucherQuery', (req, res) => {
 })
 
 // extratoPagamentos
-app.get('/statementPayments', (req, res) => {
+app.get(routes.STATEMENT_PAYMENTS, (req, res) => {
   try {
+    const { CNPJ, DATA_INI, DATA_FIM } = req.query
+
+    const data = payments.filter((p) => {
+      if (CNPJ && p.CNPJ === CNPJ) return p
+      if (DATA_INI && dateFNS.isAfter(p.DATA_INI, DATA_INI)) return p
+      if (DATA_FIM && dateFNS.isBefore(p.DATA_FIM, DATA_FIM)) return p
+      return false
+    })
+
     // const extrato_pagament = {
     //   CNPJ: '',
     //   EMPRESA: '',
@@ -99,7 +158,7 @@ app.get('/statementPayments', (req, res) => {
     //   VALOR_DESCONTO: '',
     //   IDENT_EXTRATO: '',
     // }
-    return res.status(200).send({ message: 'olá mundo' })
+    return res.status(200).send(data)
   } catch (error) {
     return res.status(500).send(error)
   }
